c265170c7f70356ab6783617cfc020d9e93b7c58
James Dutton <james@dutton.io>
2020-09-23 19:50:40 +0100

    JRD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'templates',
+    loadChildren: () =>
+      import('./templates/templates.module').then((m) => m.TemplatesModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
